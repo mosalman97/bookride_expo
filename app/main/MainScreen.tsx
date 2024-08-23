74a39321aba868c196ecfa1b91bf688bf6c5b583
Mohamed <mosalman1098@gmail.com>
2024-08-23 23:22:16 +0400

    testing

---
+++ app/main/MainScreen.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+import React from "react";
+
+const MainScreen = () => {
+  return (
+    <View>
+      <Text>MainScreen</Text>
+    </View>
+  );
+};
+
+export default MainScreen;
