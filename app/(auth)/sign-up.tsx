d2152b5f38a304c8af3b20df0c17188d76d224a7
Mohamed <mosalman1098@gmail.com>
2024-08-25 10:08:51 +0400

    api fetch wip

--- app/(auth)/sign-up.tsx
+++ app/(auth)/sign-up.tsx
@@ -8,6 +8,7 @@ import { Link, router } from "expo-router";
 import OAuth from "@/components/OAuth";
 import { useSignUp } from "@clerk/clerk-expo";
 import { ReactNativeModal } from "react-native-modal";
+import { fetchAPI } from "@/lib/fetch";
 
 const SignUp = () => {
   // local state
@@ -59,6 +60,14 @@ const SignUp = () => {
 
       if (completeSignUp.status === "complete") {
         // todo databaseuser create
+        await fetchAPI("/(api)/user", {
+          method: "POST",
+          body: JSON.stringify({
+            name: form.name,
+            email: form.email,
+            clerkId: completeSignUp.createdUserId,
+          }),
+        });
         await setActive({ session: completeSignUp.createdSessionId });
         setVerification({
           ...verification,
